40d38fcd193c59a10037bd054b5dbac4dfc5730f
Jack <jjhaek@cox.net>
2018-04-05 12:54:39 -0700

    Added Win/Lose System
    added game end scripts
    MVP nearly complete, just needs tweaking at this point... possibly add enemies and rewards

---
+++ 2D Platformer Project/Assets/Scripts/LevelComplete.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelComplete : MonoBehaviour {
+
+    public Transform player;
+
+    public float minDist;
+
+    bool playerHasLost = false;
+
+
+    private void OnTriggerEnter2D()
+    {
+        //player completes level
+        Debug.Log("Player wins");
+        //send player to next level
+        Debug.Log("Send Player to next level");
+    }
+    void Update () {
+
+        if(player.position.y < minDist && playerHasLost == false)
+        {
+            //end game player loses
+            Debug.Log("Player loses");
+            playerHasLost = true;
+            //restart level
+        }
+		
+	}
+}
